858734c9bb5bbdab07aaae515e0108418463e167
Alex Mickelson <alexmickelson96@gmail.com>
2025-01-21 21:23:21 -0700

    keyup handling

--- lib/lv_game_web/live/game_live.ex
+++ lib/lv_game_web/live/game_live.ex
@@ -8,15 +8,13 @@ defmodule LvGameWeb.GameLive do
     {:ok, socket}
   end
 
-
   def render(assigns) do
     ~H"""
     <h1>Game</h1>
-    <div phx-window-keydown="keypress">
+    <div phx-window-keydown="keypress" phx-window-keyup="keyup">
       keys pressed:
-
       <%= for key <- @keys do %>
-          <div>{key}</div>
+        <div>{key}</div>
       <% end %>
     </div>
     """
@@ -25,11 +23,23 @@ defmodule LvGameWeb.GameLive do
   def handle_event("keypress", %{"key" => key}, socket) when key in ~w(w a s d) do
     IO.puts("pressed #{key}")
     # IO.puts(socket.assigns.keys)
-    new_socket = update(socket, :keys, &(MapSet.put(&1, key)))
+    new_socket = update(socket, :keys, &MapSet.put(&1, key))
     {:noreply, new_socket}
   end
+
+  def handle_event("keyup", %{"key" => key}, socket) when key in ~w(w a s d) do
+    IO.puts("lifted key key pressed #{key}")
+    new_socket = update(socket, :keys, &MapSet.delete(&1, key))
+    {:noreply, new_socket}
+  end
+
   def handle_event("keypress", _, socket) do
     IO.puts("unknown key pressed")
+    {:noreply, socket}
+  end
+
+  def handle_event("keyup", _, socket) do
+    IO.puts("unknown key lift")
     {:noreply, socket}
   end
 end
